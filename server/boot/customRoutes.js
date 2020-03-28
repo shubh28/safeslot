ce07f0274cecba3b005ac67b1d4695b463f9e8de
Shubham Jain <shubham.jain4@oyorooms.com>
2020-03-28 18:58:05 +0530

    Init

---
+++ server/boot/customRoutes.js
@@ -0,0 +1,21 @@
+module.exports = function(app) {
+  var router = app.loopback.Router();
+  var Stores = app.models.Stores;
+  router.get('/api/stores/location', function(req, res) {
+  	var location = req.query.location;
+  	Stores.find({
+  		where: {and: [{isOperating: true}, {or:[
+  				{locality: {regex: new RegExp(`.*${location}.*`, 'i')}},
+  				{city: {regex: new RegExp(`.*${location}.*`, 'i')}}
+  			]}]}
+  	},
+  		function(err, stores) {
+	  		if (err) {
+	  			res.status(400).json(err);
+	  		} else {
+	  		    res.status(200).json(stores);
+	  		}
+	  	});
+  	});
+  app.use(router);
+};
