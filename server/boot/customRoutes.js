d5e2d24a57c7ecf8aca67f17f4642c99cb6d689c
Sanket Patel <3sanket3@gmail.com>
2020-03-31 13:46:07 +0530

    feat: add location filter to GET:stores

--- server/boot/customRoutes.js
+++ server/boot/customRoutes.js
@@ -1,22 +1,35 @@
+var loopback = require("loopback");
 module.exports = function(app) {
   var router = app.loopback.Router();
   var Stores = app.models.Stores;
-  router.get('/api/stores/location', function(req, res) {
-  	var location = req.query.location;
-  	Stores.find({
-  		where: {and: [{isOperating: true}, {or:[
-  				{locality: {regex: new RegExp(`.*${location}.*`, 'i')}},
-  				{city: {regex: new RegExp(`.*${location}.*`, 'i')}},
-          {address: {regex: new RegExp(`.*${location}.*`, 'i')}}
-  			]}]}
-  	},
-  		function(err, stores) {
-	  		if (err) {
-	  			res.status(400).json(err);
-	  		} else {
-	  		    res.status(200).json(stores);
-	  		}
-	  	});
-  	});
+  router.get("/api/stores/location", function(req, res) {
+    var location = req.query.location;
+    var lat = req.query.lat;
+    var lng = req.query.lng;
+    var userLocation = new loopback.GeoPoint({ lng: lng, lat: lat });
+    Stores.find(
+      {
+        where: {
+          and: [
+            { isOperating: true },
+            {
+              location: {
+                near: userLocation,
+                maxDistance: 1,
+                unit: "kilometers"
+              }
+            }
+          ]
+        }
+      },
+      function(err, stores) {
+       if (err) {
+          res.status(400).json(err);
+        } else {
+          res.status(200).json(stores);
+        }
+      }
+    );
+  });
   app.use(router);
 };
