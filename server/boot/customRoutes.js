6cbb79d6f1d9cf91d7cfdcaa891e9fdbaac368f2
Shubham Jain <shubham.jain4@oyorooms.com>
2020-04-01 20:00:47 +0530

    reverse support

--- server/boot/customRoutes.js
+++ server/boot/customRoutes.js
@@ -6,42 +6,61 @@ module.exports = function(app) {
     var location = req.query.location;
     var lat = req.query.lat;
     var lng = req.query.lng;
-    var userLocation = new loopback.GeoPoint({ lng: lng, lat: lat });
-    Stores.find(
-      {
-        where: {
-          and: [
-            { isOperating: true },
-            {
-              location: {
-                near: userLocation,
-                maxDistance: 10,
-                unit: "kilometers"
-              }
-            }
-          ]
-        }
-      },
-      function(err, stores) {
-        if (err) {
-          res.status(400).json(err);
-        } else {
-          const updatedStores = stores.map(store => {
-            return {
-              ...store,
-              ...{
-                distance: userLocation.distanceTo(
-                  loopback.GeoPoint(store.location),
-                  "kilometers"
-                )
-              }
-            };
-          });
 
-          res.status(200).json(updatedStores);
+    if (!lat || !lng) {
+      Stores.find({
+          where: {and: [{isOperating: true}, {or:[
+              {locality: {regex: new RegExp(`.*${location}.*`, 'i')}},
+              {city: {regex: new RegExp(`.*${location}.*`, 'i')}},
+              {address: {regex: new RegExp(`.*${location}.*`, 'i')}}
+            ]}]}
+        },
+          function(err, stores) {
+            if (err) {
+              res.status(400).json(err);
+            } else {
+                res.status(200).json(stores);
+            }
+          });
+    } else {
+      var userLocation = new loopback.GeoPoint({ lng: lng, lat: lat });
+      Stores.find(
+        {
+          where: {
+            and: [
+              { isOperating: true },
+              {
+                location: {
+                  near: userLocation,
+                  maxDistance: 10,
+                  unit: "kilometers"
+                }
+              }
+            ]
+          }
+        },
+        function(err, stores) {
+          if (err) {
+            res.status(400).json(err);
+          } else {
+            const updatedStores = stores.map(store => {
+              return {
+                ...store,
+                ...{
+                  distance: userLocation.distanceTo(
+                    loopback.GeoPoint(store.location),
+                    "kilometers"
+                  )
+                }
+              };
+            });
+
+            res.status(200).json(updatedStores);
+          }
         }
-      }
-    );
+      );
+
+    }
   });
   app.use(router);
 };
