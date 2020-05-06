71af7de4fe272835e1abb68be24febc2e31704fd
Becca Duhamel <rduhamel9@gmail.com>
2020-05-06 16:22:13 -0400

    typo

--- server/boot/customRoutes.js
+++ server/boot/customRoutes.js
@@ -124,7 +124,7 @@ module.exports = function (app) {
             booking.stores_slots &&
             booking.stores_slots() &&
             booking.stores_slots().maximun_people_allowed;
-          if (maxPeopleInSlot > 0) {
+          if (maxPeopleInSlot <= 0) {
             return res
               .status(400)
               .json({ message: "This slot is full please use another slot!" });
