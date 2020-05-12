bfbcdb9e856d26a04ce3e32f8ecdcfce4e65cff4
Omkar Rane <omkarrane10@gmail.com>
2020-05-13 03:04:40 +0530

    reverted cors changes

--- server/server.js
+++ server/server.js
@@ -10,22 +10,6 @@ const boot = require('loopback-boot');
 
 const app = module.exports = loopback();
 
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain);
-
 app.start = function () {
   // start the web server
   return app.listen(function () {
