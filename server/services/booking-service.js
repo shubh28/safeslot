f84947e5c5406a57744ec0287d00aad40fe7bfcf
Omkar Rane <omkarrane10@gmail.com>
2020-05-13 03:35:06 +0530

    field validation added

--- server/services/booking-service.js
+++ server/services/booking-service.js
@@ -67,13 +67,29 @@ module.exports.updateSlot = (app, fields, files, slotId) => {
         formattedPrescriptions[Object.keys(e)[0]] = e[Object.keys(e)[0]];
       });
 
-      booking.store_id = fields.store_id;
-      booking.slot_id = fields.slot_id;
-      booking.user_id = fields.user_id;
-      booking.status = fields.status;
-      booking.booking_date = fields.booking_date;
-      booking.order_details = fields.order_detail;
-      booking.booking_data = { prescriptions: formattedPrescriptions };
+      if (fields) {
+        if (fields.store_id)
+          booking.store_id = fields.store_id;
+
+        if (fields.slot_id)
+          booking.slot_id = fields.slot_id;
+
+        if (fields.user_id)
+          booking.user_id = fields.user_id;
+
+        if (fields.status)
+          booking.status = fields.status;
+
+        if (fields.booking_date)
+          booking.booking_date = fields.booking_date;
+
+        if (fields.order_detail)
+          booking.order_details = fields.order_detail;
+      }
+
+      if (Object.keys(formattedPrescriptions).length !== 0)
+        booking.booking_data = { prescriptions: formattedPrescriptions };
+
       return booking.save();
     })
     .catch(err => {
